e4ffb191ba1f51440f0ae9239ac22e673d88a64b
whj11200 <74046757+whj11200@users.noreply.github.com>
2025-03-04 00:11:50 +0900

    START

---
+++ Assets/Script/Test.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TestFonsion()
+    {
+        Debug.Log("´­·¶¾î¿ä");
+    }
+}
